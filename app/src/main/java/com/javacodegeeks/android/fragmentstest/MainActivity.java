115e246b45c1b82953aaf883cf271f582818114f
Brian Bansenauer <brianb@cascadia.edu>
2016-10-03 13:22:33 -0700

    initial commit

---
+++ app/src/main/java/com/javacodegeeks/android/fragmentstest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.javacodegeeks.android.fragmentstest;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      setContentView(R.layout.activity_main);
+
+	}
+
+   
+}
